fcdcef530f32a0ec729d9308bba72c749f9171f6
Jeremy Bregulla <jeremybregulla@googlemail.com>
2026-08-01 12:52:08 +0200

    Structure of App.jsx complete_II

---
+++ src/components/Services.jsx
@@ -0,0 +1,12 @@
+
+
+function Services() {           
+    return (
+        <section id="Services" className="min-h-screen bg-gray-200">
+            <h2>Unsere Dienstleistungen</h2>
+            <p>Hier werden unsere Dienstleistungen vorgestellt...</p>
+        </section>
+    );
+}
+
+export default Services
