66ef5953ea20d6bd7506fdaaac8513e13e6a6645
Jeremy Bregulla <jeremybregulla@googlemail.com>
2026-08-01 12:47:32 +0200

    Migration complete

---
+++ src/components/Connect.jsx
@@ -0,0 +1,11 @@
+    
+function Connect() {
+    return (
+    <section id="Pod" className="min-h-screen bg-gray-200">
+        <h2>Höre doch mal in unseren Podcast rein!</h2>
+        <p>Weitere multimediale Inhalte folgen...</p>
+    </section>
+    );
+}
+
+export default Connect
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+        <section className="flex justify-center dark:bg-gray-900 bg-gray-200 dark:text-white">
+            <h2>Kontakt</h2>
+            <p>[Call to Action Text]</p>
+        </section>
+    );
+}
+
+export default Footer
